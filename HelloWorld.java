802227dbd2fdcf900d4b1df871077106c0966d0a
Sylwia Łuczak-Jagieła <63807282+sylvia7sylvia@users.noreply.github.com>
2020-09-29 13:21:38 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Sylwia");
+        System.out.println(38);
+        System.out.println("Ł");
+    }
+}
