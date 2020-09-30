b9645b4daea1706456c160070e6b8c5cfd119c1e
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-09-30 14:11:07 +0200

    Dodano plik Sum.java

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String example="Hello from FirstClass";
-        System.out.println(example);
+        //String example="Hello from FirstClass";
+        System.out.println(FirstClass.sayHello());
+        //FirstClass.sayHello();
+    }
+    private static String sayHello() {
+        String text = "Dawid pierdzieli!";
+        return text;
     }
 }
