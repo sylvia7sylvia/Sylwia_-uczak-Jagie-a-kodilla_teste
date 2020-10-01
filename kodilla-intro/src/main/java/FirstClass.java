370c6503d8b5aabc4f4972256475b9eeba2b6815
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-10-01 14:39:12 +0200

    Dodano zadania z modułów 0.9 - 0.11

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,11 +1,11 @@
 public class FirstClass {
     public static void main(String[] args) {
-        //String example="Hello from FirstClass";
-        System.out.println(FirstClass.sayHello());
-        //FirstClass.sayHello();
-    }
-    private static String sayHello() {
-        String text = "Dawid pierdzieli!";
-        return text;
+        System.out.println("Hello from FirstClass!");
+
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
     }
 }
---
+++ kodilla-intro/src/main/java/MyAge.java
@@ -0,0 +1,12 @@
+public class MyAge {
+    public static void main(String[] args) {
+    int yourAge=18;
+        if (yourAge < 10){
+            System.out.println("You're a kid");
+        } else if (yourAge > 10 || yourAge < 18) {
+            System.out.println("You're a teenager");
+        } else {
+            System.out.println("You're an adult");
+        }
+    }
+}
