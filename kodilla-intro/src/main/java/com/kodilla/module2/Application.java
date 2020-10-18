a06c8b2b3e3541006b8e5437b442fb8314089803
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-10-18 10:11:52 +0200

    Moduł 2.6

---
+++ kodilla-intro/src/main/java/com/kodilla/module2/Application.java
@@ -0,0 +1,42 @@
+package com.kodilla.module2;
+
+public class Application {
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        Application adam = new Application ("Adam", 40.5, 178);
+        Application ewa = new Application ("Ewa", 28.5, 158);
+        Application kain = new Application ("Kain", 15.5, 168);
+        Application abel = new Application ("Abel", 12.5, 140);
+        Application [] humans = {adam, ewa, kain, abel};
+
+            for (int i = 0; i < humans.length; i++) {
+                if (humans[i].age > 30 && humans[i].height > 160) {
+                    System.out.println(humans[i].name + " is older than 30 and higher than 160cm");
+                } else {
+                    System.out.println(humans[i].name + " is 30 (or younger) or 160cm (or shorter)");
+                }
+    /*public static void main(String[] args) {
+        String name = "Adam";
+        double age = 40.5;
+        double height = 178;
+
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    } */
+        }
+    }
+}
--- kodilla-intro/src/main/java/com/kodilla/module2/User.java
+++ kodilla-intro/src/main/java/com/kodilla/module2/User.java
@@ -3,12 +3,12 @@ package com.kodilla.module2;
 public class User {
     String usName;
     int usAge;
-    double usHight;
+    double usHeight;
 
-    public User(String usName, int usAge, double usHight) {
+    public User(String usName, int usAge, double usHeight) {
         this.usName = usName;
         this.usAge = usAge;
-        this.usHight = usHight;
+        this.usHeight = usHeight;
     }
 
     public static void main(String[] args) {
@@ -23,22 +23,22 @@ public class User {
         User[] users = {Sylwia, Dawid, Oktawia, Anna, Natalia, Karolina, Jan};
 
         int tempAge = 0;
-        double tempHight = 0;
+        double tempHeight = 0;
         for (int i = 0; i < users.length; i++) {
             tempAge = tempAge + users[i].usAge;
-            tempHight = tempHight + users[i].usHight;
+            tempHeight = tempHeight + users[i].usHeight;
         }
         int averageAge = tempAge / users.length;
-        double averageHight = tempHight / users.length;
+        double averageHeight = tempHeight / users.length;
         System.out.println("Srednia wieku uzytkowników to " + averageAge);
-        System.out.println("Sredni wzrost uzytkowników to " + averageHight);
+        System.out.println("Sredni wzrost uzytkowników to " + averageHeight);
 
         for (int i = 0; i < users.length; i++) {  //przed zmianą - zmiana tego zapisu na inną formę jest w 40 lini
             if (users[i].usAge <= averageAge)
                 System.out.println("Uzytkownicy, ktorych wiek jest ponizej sredniej to " + users[i].usName);
         }
         for (User user : users) {   //dlaczego inteliJ tak to zmienił????
-            if (user.usHight > averageHight)
+            if (user.usHeight > averageHeight)
                 System.out.println("Uzytkownicy, wzrost jest powyżej średniej " + user.usName);
         }
     }
