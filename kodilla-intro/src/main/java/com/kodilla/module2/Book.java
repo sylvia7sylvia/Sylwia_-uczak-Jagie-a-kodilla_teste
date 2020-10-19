64a3966e6dafc6768d936ba7771e40e8147c07d6
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-10-19 20:59:44 +0200

    Modul 2.7

--- kodilla-intro/src/main/java/com/kodilla/module2/Book.java
+++ kodilla-intro/src/main/java/com/kodilla/module2/Book.java
@@ -1,35 +1,37 @@
 package com.kodilla.module2;
 
-import java.util.Scanner;
+import java.util.Scanner;  //importuje klasę Scanner
 
-public class Book {      //w klasie Book wywołujemy metodę main
-
-    private String author;
-    private String title;
+public class Book {
+    //w klasie Book wywołujemy metodę main
+    // zadeklarowane zmienne typu String (tekstowe), są prywatne żeby ich nie zmieniac
+    private final String author;
+    private final String title;
 
     private Book(String author, String title) {
+        //konstruktor Book w klasie Book
         this.author = author;
+        //ten obiekt i jego wlasciwosc
         this.title = title;
     }
 
-    @Override
-    public String toString() {
-        return this.author + " - " + this.title;
-    }
-
-    public static void main(String[] args) {   //w metodzie main wywołuje metodę of
+    public static void main(String[] args) {
+        //w metodzie main wywołuje metodę of
         String w = Book.getInput("Enter name of book author:");
         String z = Book.getInput("Enter book title:");
 
         Book book = Book.of(w, z);
-        System.out.println(book);
+        //obiekt klasy Book o właściwościach w i z
+        System.out.println(book.author + " - " + book.title);
     }
 
-    private static Book of(String author, String title) {  //w metodzie of wywołuje pywatną metodę Book (to jest konstruktor)
+    private static Book of(String author, String title) {
+        //w metodzie of wywołuje konstruktor Book i tworze nowy (new) obiekt klasy Book
         return new Book(author, title);
     }
 
-    public static String getInput(String line) {    //w metodzie getInput scaner zczytuje autora i tytuł
+    public static String getInput(String line) {
+        //w metodzie getInput scaner zczytuje autora i tytuł
         Scanner scanner = new Scanner(System.in);
         System.out.println(line);
         String result = scanner.nextLine().trim();
