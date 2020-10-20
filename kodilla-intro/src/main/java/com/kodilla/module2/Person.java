511527eb7849b92d22a265e58e99863b29712d42
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-10-20 12:13:22 +0200

    Dodano klasę Person

---
+++ kodilla-intro/src/main/java/com/kodilla/module2/Person.java
@@ -0,0 +1,14 @@
+package com.kodilla.module2;
+
+public class Person {
+    String name;
+    double age;
+    double height;
+
+    public Person(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+}
