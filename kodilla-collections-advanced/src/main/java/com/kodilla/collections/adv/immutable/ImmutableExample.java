c88de844d8571c958418914e849cfca02a8936eb
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-11-05 16:47:15 +0100

    Modul 5.3 cwiczenie - poprawki

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -2,7 +2,7 @@ package com.kodilla.collections.adv.immutable;
 
 public class ImmutableExample {
     public static void main(String[] args) {
-        Book book = new Book("Roger Penrose", "Droga do rzeczywistości");
+        Book book = new BookHacked("Roger Penrose", "Droga do rzeczywistości");
         BookHacked bookHacked = (BookHacked) book;
         bookHacked.modifyTitle("New Title");
         System.out.println(book.getTitle());
