860631a7ee1e0a93ad86cc86335e62682d6aeefb
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-12-02 18:02:01 +0100

    Modul SQL poprawki

--- kodilla-sql-modul8/zadanie_modul_8.4.sql
+++ kodilla-sql-modul8/zadanie_modul_8.4.sql
@@ -1,9 +1,16 @@
-CREATE TABLE ISSUESLISTS (
-    ID SERIAL PRIMARY KEY,
-    NAME VARCHAR(100)
+DROP TABLE IF exists issues;
+DROP TABLE IF exists issueslists;
+DROP TABLE IF exists POSTS;
+DROP TABLE IF exists users;
+
+CREATE TABLE USERS
+(
+    ID          SERIAL PRIMARY KEY,
+    FIRSTNAME   VARCHAR(100),
+    LASTNAME    VARCHAR(100)
 );
-   
-CREATE TABLE USER (
+
+CREATE TABLE ISSUESLISTS (
     ID SERIAL PRIMARY KEY,
     NAME VARCHAR(100)
 );
@@ -17,30 +24,29 @@ CREATE TABLE ISSUES (
     FOREIGN KEY (ISSUESLIST_ID)
         REFERENCES ISSUESLISTS (ID),
     FOREIGN KEY (USER_ID_ASSIGNEDTO)
-        REFERENCES USER (ID)
+        REFERENCES USERS (ID)
 );
 
-INSERT INTO USER (NAME)
-VALUES ("Sylwia"), ("Dawid"), ("Anna"), ("Oktawia");
+INSERT INTO USERS (FIRSTNAME, LASTNAME)
+VALUES ("John", "Smith"),("Zachary", "Lee"),("Stephanie", "Kovalsky"), 
+("Thomas", "Landgren"),("John", "Thomson");
 
-#select * from users;
+COMMIT;
 
 INSERT INTO ISSUESLISTS (NAME)
 VALUES ("ToDo"), ("In progress"),("Done");
 
-#select * from ISSUES;
-
 INSERT INTO ISSUES (ISSUESLIST_ID, SUMMARY, DESCRIPTION, USER_ID_ASSIGNEDTO)
-VALUES (1, "Obiad", "Zamówić burgery dla nas i frytki z batatów dla Oktawki", 4), 
-(1, "Pranie", "Poskładać pranie i poochować do szafy", 2), 
-(1, "Prasowanie", "Wyprasować sukienkę na jutro", 3), 
+VALUES (1, "Obiad", "Zamówić burgery dla nas i frytki z batatów dla Oktawki", 1), 
+(1, "Pranie", "Poskładać pranie i poochować do szafy", 1), 
+(1, "Prasowanie", "Wyprasować sukienkę na jutro", 2), 
 (1, "Kodilla", "Zrobić moduł 8.5", 1), 
-(1, "Szkoła", "Sprawdzić wiadomości na Librusie", 2), 
-(2, "Kodilla", "Skończyć zadanie domowe z modułu 8.4", 3), 
-(2, "Szkoła", "Wpisac oceny", 1), 
-(3, "Sprzątanie", "Poodkurzać mieszkanie", 2), 
-(3, "Sprzątanie", "Umyć podłogi", 3), 
-(3, "Kodilla", "Zrobić moduł 8.3", 1);
+(1, "Szkoła", "Sprawdzić wiadomości na Librusie", 3), 
+(2, "Kodilla", "Skończyć zadanie domowe z modułu 8.4", 1), 
+(2, "Szkoła", "Wpisac oceny", 4), 
+(3, "Sprzątanie", "Poodkurzać mieszkanie", 1), 
+(3, "Sprzątanie", "Umyć podłogi", 1), 
+(3, "Kodilla", "Zrobić moduł 8.3", 5);
 
 COMMIT;
 
