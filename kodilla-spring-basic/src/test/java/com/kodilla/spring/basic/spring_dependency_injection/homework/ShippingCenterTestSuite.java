19b2b97e6ba9c2f17689b662125b94ce9d8537a5
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2020-12-14 10:57:33 +0100

    Modul 10.2

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenterTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/ShippingCenterTestSuite.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class ShippingCenterTestSuite {
+public class ShippingCenterTestSuite {
 
     @Autowired
     ShippingCenter shippingCenter;
 
     @Test
     public void shouldReturnSuccessMessage() {
-        String a = "Package delivered to: Malbork";
-        assertTrue(a.equals(shippingCenter.sendPackage("Malbork", 20)));
+        String expectedText = "Package delivered to: Malbork";
+        assertTrue(expectedText.equals(shippingCenter.sendPackage("Malbork", 20)));
     }
 
     @Test
     public void shouldReturnFalseMessage() {
-        String a = "Package not delivered to: Malbork";
-        assertTrue(a.equals(shippingCenter.sendPackage("Malbork", 60)));
+        String expectedText = "Package not delivered to: Malbork";
+        assertTrue(expectedText.equals(shippingCenter.sendPackage("Malbork", 60)));
     }
 }
