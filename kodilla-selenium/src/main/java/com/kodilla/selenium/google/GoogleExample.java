0709dcd3bc446b6e422c6be2cfa4ed0e80a80344
kdrzazga <krzysztof.drzazga@op.pl>
2021-01-19 15:24:44 +0100

    Selenium - Google example

--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/GoogleExample.java
@@ -1,14 +1,45 @@
 package com.kodilla.selenium.google;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleExample {
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
-        WebDriver driver = new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        ((ChromeDriver) driver).findElementByXPath("//span[contains (text(),'Zgadzam')]").click();
-        System.out.println();
+        driver.switchTo().frame(0);
+        clickIagree(driver);
+        searchFor(driver, "Ala ma kota");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TEST PASSED");
+        driver.close();
+    }
+
+    private static void searchFor(ChromeDriver driver, String text) {
+        WebElement searchTextBox = driver.findElementByName("q");
+        searchTextBox.click();
+        searchTextBox.sendKeys(text);
+        searchTextBox.sendKeys(Keys.ENTER);
+    }
+
+    private static void clickIagree(ChromeDriver driver) {
+        String iAgreeText = "I agree";
+        try {
+            WebElement iAgreeBtn = driver.findElementByXPath("//span[contains (text(), '"+ iAgreeText + "')]");
+            System.out.println("Button text " + iAgreeBtn.getText());
+            iAgreeBtn.click();
+        } catch (Exception e) {
+            System.err.println("BUTTON NOT FOUND " + e.getLocalizedMessage());
+            driver.close();
+        }
     }
 }
