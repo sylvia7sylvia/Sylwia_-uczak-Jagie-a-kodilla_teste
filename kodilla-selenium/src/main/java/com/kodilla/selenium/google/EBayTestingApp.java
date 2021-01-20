db987dc2f7fdb9795253fae190631f20eda9e48a
Sylwia Łuczak-Jagieła <luczak.sylwia@gmail.com>
2021-01-20 11:50:32 +0100

    Zadanie 13.3 EBay

--- kodilla-selenium/src/main/java/com/kodilla/selenium/google/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/google/EBayTestingApp.java
@@ -1,39 +1,42 @@
 package com.kodilla.selenium.google;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleExample {
-
+public class EBayTestingApp {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");      // [1]
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.ebay.com/");
         driver.switchTo().frame(0);
         clickIagree(driver);
-        searchFor(driver, "Kodilla");
+        searchFor(driver, "Notebook");
 
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         System.out.println("TEST PASSED");
         driver.close();
     }
 
-    public static void searchFor(ChromeDriver driver, String text) {
-        WebElement searchTextBox = driver.findElementByName("q");
+    private static void searchFor(ChromeDriver driver, String text) {   //DLACZEGO to nie działa???
+//        WebElement searchTextBox = driver.findElementByXPath("//*[@id=\"gh-ac\"]");
+        WebElement searchTextBox = driver.findElement(By.name("_nkw"));
         searchTextBox.click();
         searchTextBox.sendKeys(text);
         searchTextBox.sendKeys(Keys.ENTER);
     }
 
-    public static void clickIagree(ChromeDriver driver) {
-        String iAgreeText = "Zgadzam się";
+    private static void clickIagree(ChromeDriver driver) { //DLACZEGO to nie działa???
+        String iAgreeText = "Accept";
         try {
-            WebElement iAgreeBtn = driver.findElementByXPath("//span[contains (text(), '" + iAgreeText + "')]");
+            WebElement iAgreeBtn = driver.findElementByName("gdpr-banner-accept" + iAgreeText + "')]");
+            //WebElement iAgreeBtn = driver.findElement(By.xpath("//*[@id=\"gdpr-banner-accept\"]" + iAgreeText));
             System.out.println("Button text " + iAgreeBtn.getText());
             iAgreeBtn.click();
         } catch (Exception e) {
